0b270315aac563f7629c3e6e10138e0910149aba
Sullivan008 <kozakpeter0424@gmail.com>
2020-01-28 17:47:41 +0100

    Implement ICalculator and Calculator Operation in Core.

---
+++ MVVMPatternInUWP.Core/Interfaces/Calculating/ICalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace MVVMPatternInUWP.Core.Interfaces.Calculating
+{
+    public interface ICalculator
+    {
+        int Add();
+
+        int Sum();
+
+        int Mul();
+
+        int Div();
+    }
+}
---
+++ MVVMPatternInUWP.Core/Operations/Calculating/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using MVVMPatternInUWP.Core.Interfaces.Calculating;
+using System;
+
+namespace MVVMPatternInUWP.Core.Operations.Calculating
+{
+    public class Calculator : ICalculator
+    {
+        private readonly int _firstValue;
+
+        private readonly int _secondValue;
+
+        public Calculator(int firstValue, int secondValue)
+        {
+            _firstValue = firstValue;
+            _secondValue = secondValue;
+        }
+
+        public int Add()
+        {
+            return _firstValue + _secondValue;
+        }
+
+        public int Sum()
+        {
+            return _firstValue - _secondValue;
+        }
+
+        public int Mul()
+        {
+            return _firstValue * _secondValue;
+        }
+
+        public int Div()
+        {
+            if (_secondValue == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed!");
+            }
+
+            return _firstValue / _secondValue;
+        }
+    }
+}
