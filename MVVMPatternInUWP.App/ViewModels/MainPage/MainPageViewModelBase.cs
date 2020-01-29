becfebf5d48f85597ffe90e0c8cfa5f0949aecc3
Sullivan008 <kozakpeter0424@gmail.com>
2020-01-29 09:53:02 +0100

    Create MainPageViewModelBase class in App

---
+++ MVVMPatternInUWP.App/ViewModels/MainPage/MainPageViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MVVMPatternInUWP.App.ViewModels.MainPage
+{
+    public abstract class MainPageViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
